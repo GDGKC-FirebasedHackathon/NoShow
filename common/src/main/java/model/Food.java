979687569a9f00fe997d453eb841dece78d3dff5
최윤호 <choiking10@naver.com>
2017-02-18 06:25:08 +0900

    음식, 어드민 계정 관련 컨트롤러 및 데이터 작성

--- common/src/main/java/model/Food.java
+++ common/src/main/java/model/Food.java
@@ -1,5 +1,14 @@
 package model;
 
+import com.google.common.hash.Hasher;
+import com.google.firebase.database.DataSnapshot;
+
+import org.apache.commons.codec.digest.Md5Crypt;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by yunho on 2017. 2. 17..
  */
@@ -8,19 +17,55 @@ public class Food {
     public String uid;
     public String name;
     public String imgurl;
-    public String price;
-    public int count;
+    public long price;
+    public long count;
 
     public Food(){}
-    public Food(String uid, String name, String price, int count) {
+    public Food(String name, long price){
+
+        this.uid = name+price;
+        this.name = name;
+        this.price = price;
+        this.count = 1;
+    }
+    public Food(String uid, String name, long price, int count) {
         this.uid = uid;
         this.name = name;
         this.price = price;
         this.count = count;
     }
-    public Food(String uid, String name, String price ) {
+    public Food(String uid, String name, long price ) {
         this.uid = uid;
         this.name = name;
         this.price = price;
     }
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+
+        result.put("name", name);
+        result.put("price", price);
+        result.put("count", count);
+        return result;
+    }
+    public String getKey(){
+        return uid;
+    }
+    public static Food parseSnapshot(DataSnapshot snapshot){
+        Food food = new Food();
+        food.uid = (String) snapshot.getKey();
+        food.name = (String) snapshot.child("name").getValue();
+        food.price = (long) snapshot.child("location").getValue();
+        food.count = (long) snapshot.child("count").getValue();
+
+        return food;
+    }
+    public static ArrayList<Food> parseSnapshotList(DataSnapshot dataSnapshot){
+        ArrayList<Food> foods = new ArrayList<>();
+        for(DataSnapshot child : dataSnapshot.getChildren()) {
+            Food food = Food.parseSnapshot(child);
+            foods.add(food);
+        }
+        return foods;
+    }
+
 }
